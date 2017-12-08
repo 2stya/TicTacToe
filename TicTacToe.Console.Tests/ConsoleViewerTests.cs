336d66b6a5a9cc5e9462c9bb281d1663702f812c
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2017-12-08 19:18:32 +0200

    Add Test to verify console look of header and footer of filed drawn

--- TicTacToe.Console.Tests/ConsoleViewerTests.cs
+++ TicTacToe.Console.Tests/ConsoleViewerTests.cs
@@ -5,13 +5,14 @@ namespace TicTacToe.Console.Tests
     public class ConsoleViewerTests
     {
         [Fact]
-        public void VerifyHeaderLookOfField()
+        public void VerifyHeaderAndFooterLookOfField()
         {
             Field field = new Field();
             ConsoleViewer viewer = new ConsoleViewer();
             string fieldDrawString = viewer.PrepareStringToDrawField(field);
 
             Assert.StartsWith("┌-┬-┬-┐\n", fieldDrawString);
+            Assert.EndsWith("└-┴-┴-┘", fieldDrawString);
         }
     }
 }
--- TicTacToe.Console/ConsoleViewer.cs
+++ TicTacToe.Console/ConsoleViewer.cs
@@ -12,12 +12,11 @@ namespace TicTacToe.Console
         public string PrepareStringToDrawField(Field field)
         {
             var marksOnField = field.GetMarksOnField().Select(i => i.ToString().Replace("Empty", " ")).ToArray();
-            return ($"┌-┬-┬-┐\n" +
+            return "┌-┬-┬-┐\n" +
                     $"|{marksOnField[0]}|{marksOnField[1]}|{marksOnField[2]}|\n" +
                     $"|{marksOnField[3]}|{marksOnField[4]}|{marksOnField[5]}|\n" +
                     $"|{marksOnField[6]}|{marksOnField[7]}|{marksOnField[8]}|\n" +
-                    $"└-┴-┴-┘");
-
+                    "└-┴-┴-┘";
         }
 
         public void DrawField(Field field)

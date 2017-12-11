a79f672290bd478dfbf5dc89aa72cde8ac8f0847
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2017-12-11 20:37:40 +0200

    Implement throwing Exception at incorrect parameter passing to HumanPlayer() constructor

--- TicTacToe/Player/HumanPlayer.cs
+++ TicTacToe/Player/HumanPlayer.cs
@@ -6,7 +6,10 @@ namespace TicTacToe.Player
     {
         public HumanPlayer(MarkType markType) : base(markType)
         {
-
+            if (markType == MarkType.Empty)
+            {
+                throw new ArgumentException("HumanPlayer can be created only with X or O MarkType parameter");
+            }
         }
 
         public override void MakeAMove()

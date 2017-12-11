d2c293f724271ff015314e8622af2f252a4446ad
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2017-12-11 20:27:47 +0200

    Add HumanPlayerTests file

---
+++ TicTacToe.Tests/HumanPlayerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TicTacToe.Player;
+using Xunit;
+
+namespace TicTacToe.Tests
+{
+    public class HumanPlayerTests
+    {
+        [Fact]
+        public void HumanPlayer_OnCreationWithEmptyMarkPassed_ThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => new HumanPlayer(MarkType.Empty));
+        }
+    }
+}

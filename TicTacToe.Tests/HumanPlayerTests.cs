1d145af78b6df853ad7edcca8f2d7456ffd88f5f
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2017-12-14 11:12:41 +0200

    Add test to verify if possible to make a move to occupied place

--- TicTacToe.Tests/HumanPlayerTests.cs
+++ TicTacToe.Tests/HumanPlayerTests.cs
@@ -7,9 +7,19 @@ namespace TicTacToe.Tests
     public class HumanPlayerTests
     {
         [Fact]
-        public void HumanPlayer_OnCreationWithEmptyMarkPassed_ThrowException()
+        public void OnCreation_WithEmptyMarkPassed_ThrowException()
         {
             Assert.Throws<ArgumentException>(() => new HumanPlayer(MarkType.Empty));
+        }
+
+        [Fact]
+        public void MakeAMove_InOccupiedPlace_ThrowException()
+        {
+            Field field = new Field();
+            HumanPlayer player = new HumanPlayer(MarkType.X);
+            player.MakeAMove(MarkPlace.BottomLeft);
+
+            Assert.Throws<ArgumentException>(() => player.MakeAMove(MarkPlace.BottomLeft));
         }
     }
 }

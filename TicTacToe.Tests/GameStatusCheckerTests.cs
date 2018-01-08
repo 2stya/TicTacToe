2a1cc3991ab858e088d4334203f593628d5a465d
Konstantin Kravchenko <konstantin.kravchenko@dfsco.com>
2018-01-08 16:51:17 +0200

    Use AAA pattern for GameStatusCheckerTests

--- TicTacToe.Tests/GameStatusCheckerTests.cs
+++ TicTacToe.Tests/GameStatusCheckerTests.cs
@@ -15,100 +15,123 @@ namespace TicTacToe.Tests
         [Fact]
         public void GetGameStatus_OnXWinsHorizontallyTop_ReturnsXWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.X, new MarkPlace(0, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.XWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnOWinsHorizontallyBottom_ReturnsOWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.O, new MarkPlace(FieldSize - 1, i));
 
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.OWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnEmptyField_ReturnsGameInProgress()
         {
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.GameInProgress, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnOWinsVerticallyLeft_ReturnsOWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.O, new MarkPlace(0, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.OWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnXWinsVerticallySecondRow_ReturnsXWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.X, new MarkPlace(1, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.XWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnOWinsVerticallyRight_ReturnsOWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.O, new MarkPlace(FieldSize - 1, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.OWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnOWinsDiagonallyRight_ReturnsOWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.O, new MarkPlace(i, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.OWins, gameStatus);
         }
 
         [Fact]
         public void GetGameStatus_OnXWinsDiagonallyLeft_ReturnsXWins()
         {
+            // Arrange
             for (int i = 0; i < FieldSize; i++)
             {
                 _field.SetMark(MarkType.X, new MarkPlace(FieldSize - 1 - i, i));
             }
 
+            // Act
             GameStatus gameStatus = GameStatusChecker.GetGameStatus(_field);
 
+            // Assert
             Assert.Equal(GameStatus.XWins, gameStatus);
         }
     }

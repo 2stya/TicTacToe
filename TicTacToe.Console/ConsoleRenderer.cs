09a1ee33c226427334f0045701570d8cdda271cd
Konstantin Kravchenko <kravchenko.konstantin@gmail.com>
2018-01-29 16:41:55 +0200

    Move methods from lazy Render class to ConsoleRenderer

--- TicTacToe.Console/ConsoleRenderer.cs
+++ TicTacToe.Console/ConsoleRenderer.cs
@@ -4,7 +4,7 @@
     {
         public virtual void DrawField(Field field)
         {
-            string stringToDrawField = Render.GetFieldRender(field);
+            string stringToDrawField = GetFieldRender(field);
             System.Console.Clear();
             System.Console.WriteLine(stringToDrawField);
         }
@@ -12,13 +12,100 @@
         public virtual void DrawLogo()
         {
             System.Console.Clear();
-            System.Console.WriteLine(Render.GetLogoRender());
+            System.Console.WriteLine(GetLogoRender());
             System.Console.Read();
         }
 
         public virtual void DrawUserInstructions()
         {
-            System.Console.WriteLine(Render.GetUserInstructionsRender());
+            System.Console.WriteLine(GetUserInstructionsRender());
+        }
+
+        protected virtual string GetFieldRender(Field field)
+        {
+            return RenderFieldTop(field.SideSize) +
+                   RenderFieldPlayArea(field) +
+                   RenderFieldBottom(field.SideSize);
+        }
+
+        protected string RenderFieldBottom(int size)
+        {
+            string fieldBottom = "└";
+            for (int i = 0; i < size; i++)
+            {
+                fieldBottom += "-";
+
+                if (i < size - 1)
+                {
+                    fieldBottom += "┴";
+                }
+            }
+
+            return fieldBottom + "┘";
+        }
+
+        protected string RenderFieldPlayArea(Field field)
+        {
+            string fieldPlayArea = "";
+
+            int size = field.SideSize;
+
+            for (int i = 0; i < size; i++)
+            {
+                fieldPlayArea += "|";
+                for (int j = 0; j < size; j++)
+                {
+                    fieldPlayArea += field.GetMark(new MarkPlace(i, j)) == MarkType.Empty ? " " : field.GetMark(new MarkPlace(i, j)).ToString();
+                    fieldPlayArea += "|";
+                }
+
+                fieldPlayArea += "\n";
+
+                if (i < size - 1)
+                {
+                    fieldPlayArea += "├-----┤\n";
+                }
+
+
+            }
+
+            return fieldPlayArea;
+        }
+
+        protected string RenderFieldTop(int size)
+        {
+            string fieldTop = "┌";
+            for (int i = 0; i < size; i++)
+            {
+                fieldTop += "-";
+
+                if (i < size - 1)
+                {
+                    fieldTop += "┬";
+                }
+            }
+
+            return fieldTop + "┐\n";
+        }
+
+        protected string GetLogoRender()
+        {
+            return @"  .-----------------------------------------------------------------.
+ /  .-.                                                         .-.  \
+|  /   \                                                       /   \  |
+| |\_.  |                    TIC TAC TOE                      |    /| |
+|\|  | /|                                                     |\  | |/|
+| `---' |             Press Enter key to continue...          | `---' |
+|       |                                                     |       | 
+|       |-----------------------------------------------------|       |
+\       |                                                     |       /
+ \     /                                                       \     /
+  `---'                                                         `---'";
+        }
+
+        protected string GetUserInstructionsRender()
+        {
+            return " 1|2|3\r\n -----\r\n 4|5|6\r\n -----\r\n 7|8|9";
         }
     }
 }
